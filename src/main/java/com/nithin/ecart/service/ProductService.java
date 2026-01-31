5e41e664f2dd40fe0379a27686558ac1181171f0
Nithin <nithinmvece@gmail.com>
2026-01-31 21:12:19 +0530

    Adding Pagination to ProductList API

--- src/main/java/com/nithin/ecart/service/ProductService.java
+++ src/main/java/com/nithin/ecart/service/ProductService.java
@@ -3,17 +3,28 @@ package com.nithin.ecart.service;
 import com.nithin.ecart.entity.Product;
 import com.nithin.ecart.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
+
+import org.springframework.data.domain.Pageable;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Service
 public class ProductService {
     @Autowired
     private ProductRepository productRepository;
 
-    public List<Product> getAllProduct(){
-       List<Product> products= productRepository.findAll();
-       return products;
+    public Map<String,Object> getAllProduct(int page, int size){
+        Pageable pageable= PageRequest.of(page,size);
+        Page<Product> products= productRepository.findAll(pageable);
+        Map<String,Object>response=new HashMap();
+        response.put("products",products.getContent());
+        response.put("totalProducts",products.getTotalElements());
+
+        return response;
     }
 }
