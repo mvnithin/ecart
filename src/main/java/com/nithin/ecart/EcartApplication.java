ef0310a5513eaf3388e644e663996e5f924dca17
Nithin <nithinmvece@gmail.com>
2026-01-31 18:27:33 +0530

    For API Testing purpose diasbling spring security

--- src/main/java/com/nithin/ecart/EcartApplication.java
+++ src/main/java/com/nithin/ecart/EcartApplication.java
@@ -3,7 +3,8 @@ package com.nithin.ecart;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+
+@SpringBootApplication()
 public class EcartApplication {
 
 	public static void main(String[] args) {
