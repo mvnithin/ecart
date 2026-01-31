3d57266e19abb0acb64aaafb321f993836f4e8f3
Nithin <nithinmvece@gmail.com>
2026-01-31 18:24:07 +0530

    ProductController Updated with getAllProductsAPI with GetMapping

--- src/main/java/com/nithin/ecart/controller/ProductController.java
+++ src/main/java/com/nithin/ecart/controller/ProductController.java
@@ -4,6 +4,7 @@ import com.nithin.ecart.entity.Product;
 import com.nithin.ecart.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,6 +16,8 @@ public class ProductController {
 
     @Autowired
     private ProductService productService;
+
+    @GetMapping
     public ResponseEntity<List<Product>> getAllProducts(){
         List<Product> products=productService.getAllProduct();
         return ResponseEntity.ok(products);
