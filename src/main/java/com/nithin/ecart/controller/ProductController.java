1b3e1f2d9c1f394fe6a11e65a420988e7bea92ef
Nithin <nithinmvece@gmail.com>
2026-01-31 18:15:16 +0530

    ProductController Created with getAllProductsAPI

--- src/main/java/com/nithin/ecart/controller/ProductController.java
+++ src/main/java/com/nithin/ecart/controller/ProductController.java
@@ -1,2 +1,22 @@
-package com.nithin.ecart.controller;public class ProductController {
+package com.nithin.ecart.controller;
+
+import com.nithin.ecart.entity.Product;
+import com.nithin.ecart.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+    public ResponseEntity<List<Product>> getAllProducts(){
+        List<Product> products=productService.getAllProduct();
+        return ResponseEntity.ok(products);
+    }
 }
