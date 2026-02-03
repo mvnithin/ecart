77c9f0e89f9dde48fc4b1d4c3bb8c719653bc25a
Nithin <nithinmvece@gmail.com>
2026-02-03 14:09:12 +0530

    CriteriaBuilder changes to cb

--- src/main/java/com/nithin/ecart/spec/ProductSpecification.java
+++ src/main/java/com/nithin/ecart/spec/ProductSpecification.java
@@ -5,14 +5,16 @@ import org.springframework.data.jpa.domain.Specification;
 
 public class ProductSpecification {
     public static Specification<Product> hasCategory(String category){
-        return ((root, query, criteriaBuilder) -> category == null? null: criteriaBuilder.equal(root.get("category"),category));
+        return ((root, query, cb) -> category == null? null: cb.equal(root.get("category"),category));
     }
     public static Specification<Product> priceBetween(Double min,Double max){
-        return (root, query, criteriaBuilder) -> {
+        return (root, query, cb) -> {
             if(min==null && max==null) return null;
-            if(min==null) return criteriaBuilder.lessThanOrEqualTo(root.get("price"),max);
-            if(max==null) return criteriaBuilder.lessThanOrEqualTo(root.get("price"),min);
-            return criteriaBuilder.between(root.get("price"),min,max);
+            if(min==null) return cb.lessThanOrEqualTo(root.get("price"),max);
+            if(max==null) return cb.lessThanOrEqualTo(root.get("price"),min);
+            return cb.between(root.get("price"),min,max);
         };
     }
+
+
 }
